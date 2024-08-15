be72c82713d3ad7d1ada1cd17f93c3f8ae1b87a4
Mr-Mani-GIT <manin6891@gmail.com>
2024-08-15 19:46:29 +0530

    CompanyDet

---
+++ src/org/client/Client.java
@@ -0,0 +1,16 @@
+package org.client;
+
+import org.company.Company;
+
+public class Client extends Company {
+	public void clientName() {
+		System.out.println("Client name : Mani");
+
+	}
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.clientName();
+		c.companyName();
+	}
+
+}
